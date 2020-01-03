7d01ea06cb05c4a571e4b51f644308b43d00d931
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-01-03 22:19:50 +0300

    Reproduced results + added iterative transformations that is more stable that origin implementation

---
+++ main.py
@@ -0,0 +1,45 @@
+import open3d
+import numpy as np
+import glob
+import copy
+
+
+def draw_registration_result(source, target):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    threshold = 0.2
+    trans_init = np.eye(4)
+    source_temp.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(
+        radius=0.5, max_nn=50))
+    target_temp.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(
+        radius=0.5, max_nn=50))
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    transformation = open3d.registration.registration_icp(source_temp, target_temp, threshold
+                                                          , trans_init,open3d.registration.TransformationEstimationPointToPlane()).transformation
+    return transformation
+
+
+# To execute: main.py <directory to pcd-files>
+if __name__ == '__main__':
+    files = glob.glob('./pcds/*.pcd')
+    print(len(files))
+    files.sort()
+    pcds = []
+    for file in files:
+        pcds.append(open3d.io.read_point_cloud(file))
+    pcd_full = []
+    source = pcds[0]
+    trans_sum = np.eye(4)
+
+    cnt = 50
+    for i in range(0, cnt):
+        print('{0} from {1}'.format(i, cnt))
+        if i in []:
+            break
+        trans = draw_registration_result(pcds[i + 1], pcds[i])
+        trans_sum = trans @ trans_sum
+        source = copy.deepcopy(pcds[i + 1]).transform(trans_sum)
+        pcd_full.append(source)
+
+    open3d.visualization.draw_geometries(pcd_full)
