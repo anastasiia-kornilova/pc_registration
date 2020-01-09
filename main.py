4ace7ac2ae88f254390e2d15250542d4a9b743da
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-01-09 22:42:16 +0300

    Improvements on accuracy for alignment

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import glob
 import copy
 import time
+from itertools import chain
 
 
 # Save pcd to png file. You have 5 seconds to rotate pcd for preferred position.
@@ -14,24 +15,21 @@ def save_pcd_to_png(name, pcd):
     vis.poll_events()
     vis.update_renderer()
     vis.run()
-    time.sleep(3)
+    time.sleep(5)
     vis.capture_screen_image(name)
     vis.destroy_window()
 
 
-def draw_registration_result(source, target):
+def find_transfomation(source, target, trans_init):
     source_temp = copy.deepcopy(source)
     target_temp = copy.deepcopy(target)
     threshold = 0.2
-    trans_init = np.eye(4)
     source_temp.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(
         radius=0.5, max_nn=50))
     target_temp.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(
         radius=0.5, max_nn=50))
-    source_temp.paint_uniform_color([1, 0.706, 0])
-    target_temp.paint_uniform_color([0, 0.651, 0.929])
     transformation = open3d.registration.registration_icp(source_temp, target_temp, threshold
-                                                          , trans_init,open3d.registration.TransformationEstimationPointToPlane()).transformation
+        , trans_init,open3d.registration.TransformationEstimationPointToPlane()).transformation
     return transformation
 
 
@@ -40,21 +38,28 @@ if __name__ == '__main__':
     files = glob.glob('./pcds/*.pcd')
     print(len(files))
     files.sort()
+
     pcds = []
+    print(files[0])
     for file in files:
         pcds.append(open3d.io.read_point_cloud(file))
-    pcd_full = []
+    print(pcds[0].has_normals())
+    print(pcds[0].has_colors())
     source = pcds[0]
-    trans_sum = np.eye(4)
+    trans_sum_approximation = np.eye(4)
 
-    cnt = 50
-    for i in range(0, cnt):
-        print('{0} from {1}'.format(i, cnt))
-        if i in []:
-            break
-        trans = draw_registration_result(pcds[i + 1], pcds[i])
-        trans_sum = trans @ trans_sum
-        source = copy.deepcopy(pcds[i + 1]).transform(trans_sum)
+    pcd_full = [pcds[0]]
+    for i in range(0, 70):
+        # if i in chain(range(51, 95), range(105, 120)):
+        #     print(i)
+        #     continue
+
+        trans = find_transfomation(pcds[i + 1], pcds[i], np.eye(4))
+        trans_sum_approximation = trans @ trans_sum_approximation
+        # Above we calculate approximation, error can be increased if we apply this transformation directly
+        # Therefore the second estimation (below) for transformation is used
+        res_trans = find_transfomation(pcds[i + 1], pcd_full[i], trans_sum_approximation)
+        source = copy.deepcopy(pcds[i + 1]).transform(res_trans)
         pcd_full.append(source)
 
     open3d.visualization.draw_geometries(pcd_full)
