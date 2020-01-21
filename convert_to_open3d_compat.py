73a832200a04e1517ec9bae7766256f2e310fd25
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-01-21 15:15:42 +0300

    Added support of accessing timestamps like colors in open3D

---
+++ convert_to_open3d_compat.py
@@ -0,0 +1,62 @@
+import sys
+import glob
+
+PCD_HEADER_SIZE = 11
+VELODYNE_MAX_INTENSITY = 255
+COLOR_PRECISION = 3
+ONE_PCD_TIME = 0.1  # Warning: this is approximation for 10Hz rotation frequency
+
+
+def convert_file(file_path, new_file_path, conv_type='timestamp'):
+    f = open(file_path, 'r')
+    new_f = open(new_file_path, 'w')
+    first_timestamp = -1
+
+    i = 0
+    for l in f:
+        if i < PCD_HEADER_SIZE:
+            if l.startswith('FIELDS'):
+                new_f.write('FIELDS x y z rgb\n')
+            elif l.startswith('SIZE'):
+                new_f.write('SIZE 4 4 4 4\n')
+            elif l.startswith('TYPE'):
+                new_f.write('TYPE F F F F\n')
+            elif l.startswith('COUNT'):
+                new_f.write('COUNT 1 1 1 1\n')
+            else:
+                new_f.write(l)
+            i += 1
+            continue
+
+        if conv_type == 'intensity':
+            parts = l.split(' ')[:-2]
+            # The next line done to get lower precision (no need in high)
+            # Maybe it can be done in beautiful way using python built-ins
+            parts[-1] = str((float(parts[-1]) * 10 ** COLOR_PRECISION
+                             // VELODYNE_MAX_INTENSITY) / 10 ** COLOR_PRECISION)
+            new_f.write(' '.join(parts) + '\n')
+        elif conv_type == 'timestamp':
+            if first_timestamp == -1:
+                first_timestamp = float(l.split(' ')[-1])
+            timestamp = (float(l.split(' ')[-1]) - first_timestamp) / ONE_PCD_TIME
+            parts = l.split(' ')[:-2]
+            parts[-1] = str(timestamp)
+            new_f.write(' '.join(parts) + '\n')
+        else:
+            print('Type {0} of conversion does not supported'.format(type))
+            break
+
+    f.close()
+    new_f.close()
+
+
+# To use: color_extractor.py <path to dir with raw pcd> <path to dir with new pcd with colors>
+if __name__ == '__main__':
+    ex_dir = sys.argv[1]
+    new_dir = sys.argv[2]
+    files = glob.glob(ex_dir + '/*.pcd')
+    files.sort()
+    i = 0
+    for file in files:
+        convert_file(file, new_dir + f'{i:03d}.pcd')
+        i += 1
