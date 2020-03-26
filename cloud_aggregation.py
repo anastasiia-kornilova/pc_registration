1297c8f3471c73d493c9721f955101efa37b7e4e
debunt <dsopichev@mail.ru>
2020-03-26 11:27:14 +0300

    Changed return type.
    Instead of list of pcd, rectify will return open3D object

--- cloud_aggregation.py
+++ cloud_aggregation.py
@@ -1,17 +1,34 @@
 import numpy as np
+path = r"mrob/lib"
+import sys
+sys.path.append(path)
 import mrob
 import copy
 import math
 import open3d
 import glob
-from main import find_transformation
+import os
+
 
 
 def rectify(source, transformation, discr_count=10):
+    """
+
+    :param source:
+    :param transformation:
+    :param discr_count:
+    :return: open3d pcd object
+    """
     timestamp_split = split_by_timestamp(source)
     discretization_splited = split_in_groups(timestamp_split, discr_count)
     aggregated_cloud = rectify_groups(discretization_splited, transformation)
-    return aggregated_cloud
+    aggregated_pcd = np.zeros((0, 3))
+    for pcd in aggregated_cloud:
+        aggregated_pcd = np.vstack((aggregated_pcd, np.asarray(pcd.points)))
+    pcd = open3d.geometry.PointCloud()
+    pcd.points = open3d.utility.Vector3dVector(aggregated_pcd)
+    # pcd.paint_uniform_color([0, 0, 0])
+    return pcd
 
 
 def rectify_groups(source_groups, transformation):
@@ -37,7 +54,7 @@ def split_in_groups(timestamps_split, discr_cnt = 10):
         xyz = []
         for j in range(ln):
             ind = i * ln + j
-            print(ind, len(timestamps_split))
+            # print(ind, len(timestamps_split))
             if ind < len(timestamps_split):
                 group = timestamps_split[ind]
                 for k in range(len(group)):
@@ -45,7 +62,7 @@ def split_in_groups(timestamps_split, discr_cnt = 10):
         arr = np.array(xyz)
         # There maybe some problems with the last array, therefore check is needed
         if len(arr) != 0:
-            print(arr)
+            # print(arr)
             pcd.points = open3d.utility.Vector3dVector(arr)
             pcds.append(pcd.paint_uniform_color([0, 0, 1]))
     return pcds
@@ -74,9 +91,16 @@ def split_by_timestamp(source):
 
     return pcd_split
 
+files = []
+path = r"data/2020-01-21/formatted" # example of folder with pcds
 
 if __name__ == '__main__':
-    files = glob.glob('./timestamps/*.pcd')
+    from main import find_transformation
+
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for file in filenames:
+            files.append(os.path.join(dirpath, file))
+
     print(len(files))
     files.sort()
     files = files[:155]
@@ -85,9 +109,6 @@ if __name__ == '__main__':
     for file in files:
         pcds.append(open3d.io.read_point_cloud(file))
 
-    transformation = find_transformation(pcds[150], pcds[151], np.eye(4))
-
-    timestamp_split = split_by_timestamp(pcds[150])
-    discretization_splited = split_in_groups(timestamp_split, discr_cnt=20)
-    aggregated_cloud = rectify_groups(discretization_splited, transformation)
-    open3d.visualization.draw_geometries(discretization_splited + aggregated_cloud)
+    transformation = find_transformation(pcds[150], pcds[152], np.eye(4))
+    aggregated_cloud = rectify(pcds[150], transformation)
+    open3d.visualization.draw_geometries([pcds[150], aggregated_cloud])
